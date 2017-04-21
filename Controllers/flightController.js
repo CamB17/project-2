e9c09b370db59634f353e142b548166765399b1c
ntutchton <ntutchton@gmail.com>
2017-04-21 14:39:38 -0600

    cleanign up the horrorshow

--- Controllers/flightController.js
+++ Controllers/flightController.js
@@ -38,157 +38,11 @@ function addUserRoutes(request, response, next){
 	db.User.findOne({ 'local.username' : request.params.username}, function(err, user){
 		console.log('USER ROUTES BEFORE: ' + user.routes);
 		getSSID(user, getIntlRoutes);
-		//console.log('USER ROUTES AFTER: ' + user.routes);
-		//user.save();
 
-
-
-
-
-
-
-
-
-// 		//clear user routes
-// 			user.routes = [];
-// 			// user.save();
-// 		//console.log("1st pass --- user's skyscanner profile: " + user.skyscanner);
-// //GET SKYSCANNER INFO -----------------------------------------------------------------
-// 		makeRequest("http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" +
-// 		 	user.location.city +"&apiKey="+ apiKeys.ssAPI + '' , function(err, response, body){
-		 	
-// 		 	var results = JSON.parse(response.body);
-// 		 	user.skyscanner.PlaceId = results.Places[0].PlaceId;
-//     		user.skyscanner.PlaceName = results.Places[0].PlaceName;
-//     		user.skyscanner.CountryId = results.Places[0].CountryId;
-//    			user.skyscanner.RegionId = results.Places[0].RegionId;
-//     		user.skyscanner.CityId = results.Places[0].CityId;
-//     		user.skyscanner.CountryName = results.Places[0].CountryName;
-// 		 	//console.log("2nd pass --- user's skyscanner profile: " + user.skyscanner);
-
-// 		 	var dates = makeDates();
-// 		 	console.log('NOT USING these generated dates: ' + dates[0] + ', ' + dates[1]);
-// 		 	console.log('using this generated ID: ' + user.skyscanner.PlaceId);
-
-// //INTERNATINOAL ROUTES --------------------------------------------------------------------		 	
-// 		 makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/" + 
-// 		 		user.skyscanner.PlaceId + "/anywhere/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
-// 		 		apiKeys.ssAPI + '' , function(err, response, body){
-// 		 			//ALL FLIGHT DATA FROM DENVER TO COUNTRIES
-// 	 			var flights = JSON.parse(body);
-// 	 				//console.log('resp: '+ flights.Places[0].Name); //√ works
-// 	 				//console.log('resp: '+ flights.Carriers[0].Name); //√ works
-// 	 				//console.log('quotes: '+ flights.Quotes[0].MinPrice); //√ works
-	 				
-// 	 			var newRoutes =[];
-
-// 	 			console.log('number of international quotes: ' + flights.Quotes.length);
-// 				flights.Quotes.forEach(function(quote) {
-
-// 					var newRoute = {
-// 					price : quote.MinPrice,
-// 					destination : quote.OutboundLeg.DestinationId,
-// 					inbound : quote.InboundLeg.DepartureDate,
-// 					direct : quote.Direct,
-// 					outbound : quote.OutboundLeg.DepartureDate
-// 					//carrier???
-// 					};
-
-// 					newRoutes.push(newRoute);
-// 				});
-
-// 				//fill newRoutes with international routes
-// 				newRoutes.forEach(function(newRoute){
-					
-// 					flights.Places.forEach(function(place){
-// 						if (newRoute.destination === place.PlaceId){
-// 							newRoute.destination = place.Name;
-// 						}
-// 					});
-// 					user.routes.push(newRoute);
-// 				});
-
-// 				//user.save();
-// 				//templates.forEach(template => {console.log(template.destination);}); 
-// 		 		//console.log(user.routes);
-// 		 	});//end request for international flight data
-// //NATIONAL ROUTES ----------------------------------------------------------------------------------
-// 			makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/"+ 
-// 		 		user.skyscanner.PlaceId + "/US/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
-// 		 		apiKeys.ssAPI + '' , function(err, response, body){
-		 		
-// 		 		var flights = JSON.parse(body);
-// 		 			//console.log('body: \n' + flights.Quotes);
-
-// 		 		var newRoutes =[];
-
-// 	 			console.log('number of local quotes: ' + flights.Quotes.length);
-// 				flights.Quotes.forEach(function(quote) {
-
-// 					var newRoute = {
-// 					price : quote.MinPrice,
-// 					destination : quote.OutboundLeg.DestinationId,
-// 					inbound : quote.InboundLeg.DepartureDate,
-// 					direct : quote.Direct,
-// 					outbound : quote.OutboundLeg.DepartureDate
-// 					//carrier???
-// 					};
-
-// 					newRoutes.push(newRoute);
-// 				});
-
-// 				newRoutes.forEach(function(newRoute){
-					
-// 					flights.Places.forEach(function(place){
-// 						if (newRoute.destination === place.PlaceId){
-// 							newRoute.destination = place.Name;
-// 						}
-// 					});
-// 				user.routes.push(newRoute);
-// 				});
-// 				// user.save();
-// 				// console.log(user.routes);
-// //TEMPURATURES DATAZ -----------------------------------------------------------------
-// 		var flightRoutes = user.routes;
-// 		flightRoutes.forEach(function(route){
-
-// 			//get destinations in readable api format
-// 			var destinationString = route.destination.split(' ').join('+');
-// 			makeRequest('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + destinationString +
-// 			 ',' + /* should insert country codes here */ '&units=imperial&cnt=' + /* days of weather needed */'3' + 
-// 			 '&apiKey=' + apiKeys.owmAPI, function(err, response, body){
-// 			 	var fullTempData = JSON.parse(body);
-// 			 	//console.log('TEMP DATA FOR: ' + route.destination +': \n' + fullTempData); works √
-
-// 			 	var temps = fullTempData.list;
-// 			 	var tempsArray = [];
-// 			 	temps.forEach(function(temp){
-// 			 		tempsArray.push(temp.temp.day);
-// 			 	});
-// 			 	//console.log(tempsArray);
-// 			 	//average temps
-// 			 	var total = 0;
-// 			 	for(i=0; i < tempsArray.length; i ++){
-// 			 		total += tempsArray[i];
-// 			 	}
-// 			 	total = total/tempsArray.length;
-// 	console.log('average temp in ' + route.destination + '  is '+ total);
-// 			 	route.temperature = total;
-			
-// 			user.save();
-// 			});//weather request
-// 			});
-// 		 	});//end request national flight data
-// 		});//end request for sckyscanner Id names
-// //ADD TEMPURATURES TO ROUTES
-// 	user.save();
-// 	console.log(user.routes);
 	});//end find user
 }//end route
 
-
-
-
+//---------------------------NON-ROUTE FUNCITONS BELOW-----------------
 function getSSID(user, next){
 	makeRequest("http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" +
 		 user.location.city +"&apiKey="+ apiKeys.ssAPI + '' , function(err, response, body){
@@ -249,11 +103,23 @@ console.log('number of international quotes: ' + flights.Quotes.length);
 			flights.Places.forEach(function(place){
 				if (newRoute.destination === place.PlaceId){
 					newRoute.destination = place.Name;
-					getRouteTemp(newRoute);
+					//setter function for newRoute.temp
+					function incomingTemp(temp){newRoute.temperature = temp;}
+					getRouteTemp(newRoute, incomingTemp);
 				}
+
 			});
-			//store newRoute in user
-			user.routes.push(newRoute);
+		//checks if newRoute.temp is set, if it is, sets and saves the newRoute to user
+		var interval = setInterval(function(){
+			if (newRoute.temperature){
+				console.log("Set Temp for Int'l route: " + newRoute.destination + ": "+ newRoute.temperature );
+				user.routes.push(newRoute);
+				if(user.routes.length === newRoutes.length){
+					user.save();
+				}
+				clearInterval(interval);
+			}
+		}, 500);
 		});
 	//user.save();
 	next(user);
@@ -304,6 +170,7 @@ console.log('number of national quotes: ' + flights.Quotes.length);
 			var interval = setInterval(function(){
 				if (newRoute.temperature){
 					user.routes.push(newRoute);
+					console.log("Set Temp for National route: " + newRoute.destination + ": "+ newRoute.temperature );
 					if(user.routes.length === newRoutes.length){
 						user.save();
 					}
@@ -317,7 +184,7 @@ console.log('number of national quotes: ' + flights.Quotes.length);
 }
 
 function getRouteTemp(newRoute, incomingTemp){
-	console.log('getting temp'+ newRoute.destination);
+	//console.log('getting temp'+ newRoute.destination);
 	
 	//get destinations in readable api format
 	var destinationString = newRoute.destination.split(' ').join('+');
@@ -326,7 +193,7 @@ function getRouteTemp(newRoute, incomingTemp){
 		 '&apiKey=' + apiKeys.owmAPI, function(err, response, body){
 		 var total = 6;
 		 var fullTempData = JSON.parse(body);
-		 console.log('response:' + fullTempData);
+		 //console.log('response:' + fullTempData);
 
 //IMPLEMENT .MAP BELOW
 		 var temps = fullTempData.list;
@@ -340,21 +207,8 @@ function getRouteTemp(newRoute, incomingTemp){
 		 	}
 		 	total = total/tempsArray.length;
 		  incomingTemp(total);
-
-		});//weather request
+	});//weather request
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 function makeDates(){
 	//this function makes dates, but will NOT work around new Years' time
@@ -366,7 +220,7 @@ function makeDates(){
 	var offset = d.getDay() - 5; //moving current day to friday
 	var departureDay = d.getDate() - offset; 
 //next weekend
-//	departureDay += 7;
+	departureDay += 7;
 	if (offset > 0) {departureDay += 7;}//date of next friday
 	//find days in month
 	if (d.getMonth() === 0 || d.getMonth() === 2 || d.getMonth() === 4 || 
@@ -380,7 +234,7 @@ function makeDates(){
 	//find return day, also check for end of month
 	var returnDay = departureDay + 2;
 //next weekend
-//	returnDay += 7;
+	returnDay += 7;
 	if (returnDay > daysInMonth) {returnDay -= daysInMonth; returnMonth++;}
 
 	if (departureDay < 10) {departureDay = '0' + String(departureDay);}
