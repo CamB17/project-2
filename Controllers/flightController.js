e4f4e9dd1c0f633a8fd4fb1aca96be3ca1b3ba40
ntutchton <ntutchton@gmail.com>
2017-04-21 13:41:10 -0600

    function hell

--- Controllers/flightController.js
+++ Controllers/flightController.js
@@ -36,148 +36,304 @@ function addUserRoutes(request, response, next){
 	console.log('received this username: ' + request.params.username);
 	//get user
 	db.User.findOne({ 'local.username' : request.params.username}, function(err, user){
-		
-		//clear user routes
-			user.routes = [];
-			// user.save();
-		//console.log("1st pass --- user's skyscanner profile: " + user.skyscanner);
-//GET SKYSCANNER INFO -----------------------------------------------------------------
-		makeRequest("http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" +
-		 	user.location.city +"&apiKey="+ apiKeys.ssAPI + '' , function(err, response, body){
+		console.log('USER ROUTES BEFORE: ' + user.routes);
+		getSSID(user, getIntlRoutes);
+		//console.log('USER ROUTES AFTER: ' + user.routes);
+		//user.save();
+
+
+
+
+
+
+
+
+
+// 		//clear user routes
+// 			user.routes = [];
+// 			// user.save();
+// 		//console.log("1st pass --- user's skyscanner profile: " + user.skyscanner);
+// //GET SKYSCANNER INFO -----------------------------------------------------------------
+// 		makeRequest("http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" +
+// 		 	user.location.city +"&apiKey="+ apiKeys.ssAPI + '' , function(err, response, body){
 		 	
-		 	var results = JSON.parse(response.body);
-		 	user.skyscanner.PlaceId = results.Places[0].PlaceId;
-    		user.skyscanner.PlaceName = results.Places[0].PlaceName;
-    		user.skyscanner.CountryId = results.Places[0].CountryId;
-   			user.skyscanner.RegionId = results.Places[0].RegionId;
-    		user.skyscanner.CityId = results.Places[0].CityId;
-    		user.skyscanner.CountryName = results.Places[0].CountryName;
-		 	//console.log("2nd pass --- user's skyscanner profile: " + user.skyscanner);
+// 		 	var results = JSON.parse(response.body);
+// 		 	user.skyscanner.PlaceId = results.Places[0].PlaceId;
+//     		user.skyscanner.PlaceName = results.Places[0].PlaceName;
+//     		user.skyscanner.CountryId = results.Places[0].CountryId;
+//    			user.skyscanner.RegionId = results.Places[0].RegionId;
+//     		user.skyscanner.CityId = results.Places[0].CityId;
+//     		user.skyscanner.CountryName = results.Places[0].CountryName;
+// 		 	//console.log("2nd pass --- user's skyscanner profile: " + user.skyscanner);
 
-		 	var dates = makeDates();
-		 	console.log('NOT USING these generated dates: ' + dates[0] + ', ' + dates[1]);
-		 	console.log('using this generated ID: ' + user.skyscanner.PlaceId);
+// 		 	var dates = makeDates();
+// 		 	console.log('NOT USING these generated dates: ' + dates[0] + ', ' + dates[1]);
+// 		 	console.log('using this generated ID: ' + user.skyscanner.PlaceId);
 
-//INTERNATINOAL ROUTES --------------------------------------------------------------------		 	
-		 makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/" + 
-		 		user.skyscanner.PlaceId + "/anywhere/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
-		 		apiKeys.ssAPI + '' , function(err, response, body){
-		 			//ALL FLIGHT DATA FROM DENVER TO COUNTRIES
-	 			var flights = JSON.parse(body);
-	 				//console.log('resp: '+ flights.Places[0].Name); //√ works
-	 				//console.log('resp: '+ flights.Carriers[0].Name); //√ works
-	 				//console.log('quotes: '+ flights.Quotes[0].MinPrice); //√ works
+// //INTERNATINOAL ROUTES --------------------------------------------------------------------		 	
+// 		 makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/" + 
+// 		 		user.skyscanner.PlaceId + "/anywhere/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
+// 		 		apiKeys.ssAPI + '' , function(err, response, body){
+// 		 			//ALL FLIGHT DATA FROM DENVER TO COUNTRIES
+// 	 			var flights = JSON.parse(body);
+// 	 				//console.log('resp: '+ flights.Places[0].Name); //√ works
+// 	 				//console.log('resp: '+ flights.Carriers[0].Name); //√ works
+// 	 				//console.log('quotes: '+ flights.Quotes[0].MinPrice); //√ works
 	 				
-	 			var newRoutes =[];
+// 	 			var newRoutes =[];
 
-	 			console.log('number of international quotes: ' + flights.Quotes.length);
-				flights.Quotes.forEach(function(quote) {
+// 	 			console.log('number of international quotes: ' + flights.Quotes.length);
+// 				flights.Quotes.forEach(function(quote) {
 
-					var newRoute = {
-					price : quote.MinPrice,
-					destination : quote.OutboundLeg.DestinationId,
-					inbound : quote.InboundLeg.DepartureDate,
-					direct : quote.Direct,
-					outbound : quote.OutboundLeg.DepartureDate
-					//carrier???
-					};
+// 					var newRoute = {
+// 					price : quote.MinPrice,
+// 					destination : quote.OutboundLeg.DestinationId,
+// 					inbound : quote.InboundLeg.DepartureDate,
+// 					direct : quote.Direct,
+// 					outbound : quote.OutboundLeg.DepartureDate
+// 					//carrier???
+// 					};
 
-					newRoutes.push(newRoute);
-				});
+// 					newRoutes.push(newRoute);
+// 				});
 
-				//fill newRoutes with international routes
-				newRoutes.forEach(function(newRoute){
+// 				//fill newRoutes with international routes
+// 				newRoutes.forEach(function(newRoute){
 					
-					flights.Places.forEach(function(place){
-						if (newRoute.destination === place.PlaceId){
-							newRoute.destination = place.Name;
-						}
-					});
-					user.routes.push(newRoute);
-				});
+// 					flights.Places.forEach(function(place){
+// 						if (newRoute.destination === place.PlaceId){
+// 							newRoute.destination = place.Name;
+// 						}
+// 					});
+// 					user.routes.push(newRoute);
+// 				});
 
-				//user.save();
-				//templates.forEach(template => {console.log(template.destination);}); 
-		 		//console.log(user.routes);
-		 	});//end request for international flight data
-//NATIONAL ROUTES ----------------------------------------------------------------------------------
-			makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/"+ 
-		 		user.skyscanner.PlaceId + "/US/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
-		 		apiKeys.ssAPI + '' , function(err, response, body){
+// 				//user.save();
+// 				//templates.forEach(template => {console.log(template.destination);}); 
+// 		 		//console.log(user.routes);
+// 		 	});//end request for international flight data
+// //NATIONAL ROUTES ----------------------------------------------------------------------------------
+// 			makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/"+ 
+// 		 		user.skyscanner.PlaceId + "/US/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
+// 		 		apiKeys.ssAPI + '' , function(err, response, body){
 		 		
-		 		var flights = JSON.parse(body);
-		 			//console.log('body: \n' + flights.Quotes);
+// 		 		var flights = JSON.parse(body);
+// 		 			//console.log('body: \n' + flights.Quotes);
 
-		 		var newRoutes =[];
+// 		 		var newRoutes =[];
 
-	 			console.log('number of local quotes: ' + flights.Quotes.length);
-				flights.Quotes.forEach(function(quote) {
+// 	 			console.log('number of local quotes: ' + flights.Quotes.length);
+// 				flights.Quotes.forEach(function(quote) {
 
-					var newRoute = {
-					price : quote.MinPrice,
-					destination : quote.OutboundLeg.DestinationId,
-					inbound : quote.InboundLeg.DepartureDate,
-					direct : quote.Direct,
-					outbound : quote.OutboundLeg.DepartureDate
-					//carrier???
-					};
+// 					var newRoute = {
+// 					price : quote.MinPrice,
+// 					destination : quote.OutboundLeg.DestinationId,
+// 					inbound : quote.InboundLeg.DepartureDate,
+// 					direct : quote.Direct,
+// 					outbound : quote.OutboundLeg.DepartureDate
+// 					//carrier???
+// 					};
 
-					newRoutes.push(newRoute);
-				});
+// 					newRoutes.push(newRoute);
+// 				});
 
-				newRoutes.forEach(function(newRoute){
+// 				newRoutes.forEach(function(newRoute){
 					
-					flights.Places.forEach(function(place){
-						if (newRoute.destination === place.PlaceId){
-							newRoute.destination = place.Name;
-						}
-					});
-				user.routes.push(newRoute);
-				});
-				// user.save();
-				// console.log(user.routes);
-//TEMPURATURES DATAZ -----------------------------------------------------------------
-		var flightRoutes = user.routes;
-		flightRoutes.forEach(function(route){
+// 					flights.Places.forEach(function(place){
+// 						if (newRoute.destination === place.PlaceId){
+// 							newRoute.destination = place.Name;
+// 						}
+// 					});
+// 				user.routes.push(newRoute);
+// 				});
+// 				// user.save();
+// 				// console.log(user.routes);
+// //TEMPURATURES DATAZ -----------------------------------------------------------------
+// 		var flightRoutes = user.routes;
+// 		flightRoutes.forEach(function(route){
 
-			//get destinations in readable api format
-			var destinationString = route.destination.split(' ').join('+');
-			makeRequest('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + destinationString +
-			 ',' + /* should insert country codes here */ '&units=imperial&cnt=' + /* days of weather needed */'3' + 
-			 '&apiKey=' + apiKeys.owmAPI, function(err, response, body){
-			 	var fullTempData = JSON.parse(body);
-			 	//console.log('TEMP DATA FOR: ' + route.destination +': \n' + fullTempData); works √
+// 			//get destinations in readable api format
+// 			var destinationString = route.destination.split(' ').join('+');
+// 			makeRequest('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + destinationString +
+// 			 ',' + /* should insert country codes here */ '&units=imperial&cnt=' + /* days of weather needed */'3' + 
+// 			 '&apiKey=' + apiKeys.owmAPI, function(err, response, body){
+// 			 	var fullTempData = JSON.parse(body);
+// 			 	//console.log('TEMP DATA FOR: ' + route.destination +': \n' + fullTempData); works √
 
-			 	var temps = fullTempData.list;
-			 	var tempsArray = [];
-			 	temps.forEach(function(temp){
-			 		tempsArray.push(temp.temp.day);
-			 	});
-			 	//console.log(tempsArray);
-			 	//average temps
-			 	var total = 0;
-			 	for(i=0; i < tempsArray.length; i ++){
-			 		total += tempsArray[i];
-			 	}
-			 	total = total/tempsArray.length;
-	console.log('average temp in ' + route.destination + '  is '+ total);
-			 	route.temperature = total;
+// 			 	var temps = fullTempData.list;
+// 			 	var tempsArray = [];
+// 			 	temps.forEach(function(temp){
+// 			 		tempsArray.push(temp.temp.day);
+// 			 	});
+// 			 	//console.log(tempsArray);
+// 			 	//average temps
+// 			 	var total = 0;
+// 			 	for(i=0; i < tempsArray.length; i ++){
+// 			 		total += tempsArray[i];
+// 			 	}
+// 			 	total = total/tempsArray.length;
+// 	console.log('average temp in ' + route.destination + '  is '+ total);
+// 			 	route.temperature = total;
 			
-			user.save();
-			});//weather request
-			});
-		 	});//end request national flight data
-		});//end request for sckyscanner Id names
-//ADD TEMPURATURES TO ROUTES
-	user.save();
-	console.log(user.routes);
+// 			user.save();
+// 			});//weather request
+// 			});
+// 		 	});//end request national flight data
+// 		});//end request for sckyscanner Id names
+// //ADD TEMPURATURES TO ROUTES
+// 	user.save();
+// 	console.log(user.routes);
 	});//end find user
 }//end route
 
 
 
 
+function getSSID(user, next){
+	makeRequest("http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" +
+		 user.location.city +"&apiKey="+ apiKeys.ssAPI + '' , function(err, response, body){
+		 	
+		 var results = JSON.parse(response.body);
 
+	 	user.skyscanner.PlaceId = results.Places[0].PlaceId;
+		user.skyscanner.PlaceName = results.Places[0].PlaceName;
+		user.skyscanner.CountryId = results.Places[0].CountryId;
+			user.skyscanner.RegionId = results.Places[0].RegionId;
+		user.skyscanner.CityId = results.Places[0].CityId;
+		user.skyscanner.CountryName = results.Places[0].CountryName;
+//console.log("2nd pass --- user's skyscanner profile: " + user.skyscanner);
+		 //user.save();
+		 next(user, getNatlRoutes);
+		 });
+}
+
+function getIntlRoutes(user, next){
+
+	//clear user routes
+
+	user.routes = [];
+
+ 	var dates = makeDates();
+console.log("USING these generated dates for Int'l routes: " + dates[0] + ', ' + dates[1]);
+console.log("using this generated ID for Int'l routes: " + user.skyscanner.PlaceId);
+
+//INTERNATINOAL ROUTES --------------------------------------------------------------------		 	
+ 	makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/" + 
+ 		user.skyscanner.PlaceId + "/anywhere/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
+ 		apiKeys.ssAPI + '' , function(err, response, body){
+
+		var flights = JSON.parse(body);
+		//console.log('resp: '+ flights.Places[0].Name); //√ works
+		//console.log('resp: '+ flights.Carriers[0].Name); //√ works
+		//console.log('quotes: '+ flights.Quotes[0].MinPrice); //√ works
+				
+		var newRoutes =[];
+
+console.log('number of international quotes: ' + flights.Quotes.length);
+		//store each flight in array newRoutes[]	
+		flights.Quotes.forEach(function(quote) {
+
+			var newRoute = {
+			price : quote.MinPrice,
+			destination : quote.OutboundLeg.DestinationId,
+			inbound : quote.InboundLeg.DepartureDate,
+			direct : quote.Direct,
+			outbound : quote.OutboundLeg.DepartureDate
+			//carrier???
+			};
+			newRoutes.push(newRoute);
+		});
+
+		//change newRoute names
+		newRoutes.forEach(function(newRoute){	
+			flights.Places.forEach(function(place){
+				if (newRoute.destination === place.PlaceId){
+					newRoute.destination = place.Name;
+					getRouteTemp(place.Name, newRoute);
+				}
+			});
+			//store newRoute in user
+			user.routes.push(newRoute);
+		});
+	//user.save();
+	next(user);
+ 	});//end request for international flight data	
+}
+
+function getNatlRoutes(user){
+	var dates = makeDates();
+console.log("USING these generated dates for Natinoal routes: " + dates[0] + ', ' + dates[1]);
+console.log("using this generated ID for National routes: " + user.skyscanner.PlaceId);
+
+	makeRequest("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/"+ 
+ 		user.skyscanner.PlaceId + "/US/" + dates[0] + "/" + dates[0] + "?apiKey=" + 
+ 		apiKeys.ssAPI + '' , function(err, response, body){
+ 		
+ 		var flights = JSON.parse(body);
+ 		
+ 		var newRoutes =[];
+
+console.log('number of national quotes: ' + flights.Quotes.length);
+		//sotre ecah flight in newRoutes[]		
+		flights.Quotes.forEach(function(quote) {
+
+			var newRoute = {
+			price : quote.MinPrice,
+			destination : quote.OutboundLeg.DestinationId,
+			inbound : quote.InboundLeg.DepartureDate,
+			direct : quote.Direct,
+			outbound : quote.OutboundLeg.DepartureDate
+			//carrier???
+			};
+
+			newRoutes.push(newRoute);
+		});
+
+		//change newroute names and set temps
+		newRoutes.forEach(function(newRoute){
+			flights.Places.forEach(function(place){
+				if (newRoute.destination === place.PlaceId){
+					newRoute.destination = place.Name;
+					console.log('trying to find a temperature for: '+ place.Name);
+					getRouteTemp(place.Name, newRoute);
+					console.log('set a temperature of: '+ getRouteTemp(place.Name)+ ' for '+ place.Name);
+				}
+
+			});
+		//storeNewRoute in user
+		user.routes.push(newRoute);
+		});
+
+		user.save();
+	});
+}
+
+function getRouteTemp(destination, route){
+	
+	//get destinations in readable api format
+	var destinationString = destination.split(' ').join('+');
+	makeRequest('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + destinationString +
+		 ',' + /* should insert country codes here */ '&units=imperial&cnt=' + /* days of weather needed */'3' + 
+		 '&apiKey=' + apiKeys.owmAPI, function(err, response, body){
+		 var total = 6;
+		 var fullTempData = JSON.parse(body);
+
+//IMPLEMENT .MAP BELOW
+		 var temps = fullTempData.list;
+		 var tempsArray = [];
+		 	temps.forEach(function(temp){
+		 		tempsArray.push(temp.temp.day);
+		 	});
+		 	//average temps
+		 	for(i=0; i < tempsArray.length; i ++){
+		 		total += tempsArray[i];
+		 	}
+
+		 	total = total/tempsArray.length;
+//console.log('average temp in ' + destination + '  is '+ total);	
+		return total;
+		});//weather request
+}
 
 
 
